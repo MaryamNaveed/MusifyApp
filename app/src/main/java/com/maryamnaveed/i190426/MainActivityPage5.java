a06ce881ce22b5cafce6932f8425d5a78bbae58d
MaryamNaveed <maryamnaveed0208@gmail.com>
2022-09-17 14:28:37 +0500

    Commit

--- app/src/main/java/com/maryamnaveed/i190426/MainActivityPage5.java
+++ app/src/main/java/com/maryamnaveed/i190426/MainActivityPage5.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivityPage5 extends AppCompatActivity {
+
+    BottomNavigationView mNavigationBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page5);
+
+        mNavigationBottom=findViewById(R.id.mNavigationBottom);
+
+        mNavigationBottom.getMenu().setGroupCheckable(0, true, false);
+        for (int i=0; i<mNavigationBottom.getMenu().size(); i++) {
+            mNavigationBottom.getMenu().getItem(i).setChecked(false);
+        }
+        mNavigationBottom.getMenu().setGroupCheckable(0, true, true);
     }
 }
--- app/src/main/java/com/maryamnaveed/i190426/MainActivityPage6.java
+++ app/src/main/java/com/maryamnaveed/i190426/MainActivityPage6.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivityPage6 extends AppCompatActivity {
+
+    BottomNavigationView mNavigationBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page6);
+
+        mNavigationBottom=findViewById(R.id.mNavigationBottom);
+
+        mNavigationBottom.getMenu().setGroupCheckable(0, true, false);
+        for (int i=0; i<mNavigationBottom.getMenu().size(); i++) {
+            mNavigationBottom.getMenu().getItem(i).setChecked(false);
+        }
+        mNavigationBottom.getMenu().setGroupCheckable(0, true, true);
     }
 }
